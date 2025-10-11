33c753fcbbff70df48df704209e39a6244f25dd1
Gurpinderjeet Sandhu <psandhu0124@gmail.com>
2025-10-11 00:19:39 -0400

    Finished Cart Route

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getCartProducts, addToCart, removeAllFromCart, updateQuantity } from "../controller/cart.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
+export default router;
