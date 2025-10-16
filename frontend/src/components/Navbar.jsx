7b5c2927bec8e25860ea3449303409b8e625dc32
Gurpinderjeet Sandhu <psandhu0124@gmail.com>
2025-10-15 22:15:30 -0400

    Starting to setup React and Building Out Frontend Design

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+        
+    </header>
+  )
+}
+
+export default Navbar
